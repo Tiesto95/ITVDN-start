579f9a85b1dcd01f1654dee77b7cbafb42859bd5
Беслан <bes-zaurbaev@yandex.ru>
2022-10-27 23:47:05 +0300

    Decorator test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello Word!')
+import time
+from functools import wraps
+
+def time_decor(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        time_result = time.time()
+        func(*args, **kwargs)
+        print(time.time() - time_result)
+
+    return wrap
+
+
+@time_decor
+def test(sleep):
+    time.sleep(sleep)
+
+
+test(sleep=5)
